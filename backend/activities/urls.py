cc27255f67a1215826522940f6a51ee6b2d9b08b
medkaad <kardalm132@gmail.com>
2026-03-11 21:40:44 +0100

    style: fix import spacing in activities api

--- backend/activities/urls.py
+++ backend/activities/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from .views import ActivityListCreateView
 
-
 urlpatterns = [
     path("activities", ActivityListCreateView.as_view(), name="activity-list-create"),
 ]
