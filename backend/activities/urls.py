b3abf47d83594e66fb6f05191598bab2d1d5ccd2
medkaad <kardalm132@gmail.com>
2026-03-11 21:12:08 +0100

    style: fix import order in activities views

--- backend/activities/urls.py
+++ backend/activities/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
 from .views import ActivityListCreateView
+
 
 urlpatterns = [
     path("activities", ActivityListCreateView.as_view(), name="activity-list-create"),
