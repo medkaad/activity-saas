80c2d9f7e7de8ab044b0c2a08e8dafd2d37ac15c
medkaad <kardalm132@gmail.com>
2026-03-16 15:46:00 +0100

    fix import

--- backend/activities/urls.py
+++ backend/activities/urls.py
@@ -1,9 +1,25 @@
 from django.urls import path
 
-from .views import ActivityPDFView, ActivityRetrieveView, ActivityListCreateView
+from .views import (
+    ActivityListCreateView,
+    ActivityRetrieveView,
+    ActivityPDFView
+)
 
 urlpatterns = [
-    path("activities", ActivityListCreateView.as_view(), name="activity-list-create"),
-    path("activities/<int:pk>", ActivityRetrieveView.as_view(), name="activity-detail"),
-    path("activities/<int:pk>/pdf", ActivityPDFView.as_view(), name="activity-pdf"),
+    path(
+        "activities/",
+        ActivityListCreateView.as_view(),
+        name="activity-list-create",
+    ),
+    path(
+        "activities/<int:pk>/",
+        ActivityRetrieveView.as_view(),
+        name="activity-detail",
+    ),
+    path(
+        "activities/<int:pk>/pdf/",
+        ActivityPDFView.as_view(),
+        name="activity-pdf",
+    ),
 ]
