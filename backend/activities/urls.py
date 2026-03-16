28d0b9e8a481a7be4b9e6ceccb9fb98741a68aa7
medkaad <kardalm132@gmail.com>
2026-03-16 15:34:00 +0100

    fix import

--- backend/activities/urls.py
+++ backend/activities/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ActivityListCreateView, ActivityRetrieveView, ActivityPDFView
+from .views import ActivityPDFView, ActivityRetrieveView, ActivityListCreateView
 
 urlpatterns = [
     path("activities", ActivityListCreateView.as_view(), name="activity-list-create"),
